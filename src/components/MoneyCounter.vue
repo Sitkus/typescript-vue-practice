251a97025e0ed1e3e42d9437c41bdcf11dab9741
Lukas Sitkus <luk@box4metal.com>
2021-09-09 20:59:38 +0300

    Quick playground with count clicker and typescript

---
+++ src/components/MoneyCounter.vue
@@ -0,0 +1,43 @@
+<template>
+    <section>
+        <h5>Money counter: {{ money }}</h5>
+        <button @click="checkMoneyCount('increment')">+++</button>
+        <button @click="checkMoneyCount('decrement')">---</button>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'MoneyCounter',
+
+    data() {
+        return {
+            money: 10
+        };
+    },
+
+    methods: {
+        checkMoneyCount(mutation: string): void {
+            if (this.money === 0 && mutation == 'decrement') {
+                console.log('Maaan you are at zero already!');
+
+                return;
+            } else if (mutation == 'increment' || mutation == 'decrement') {
+                this[mutation]();
+            }
+        },
+
+        increment(): void {
+            this.money++;
+        },
+
+        decrement(): void {
+            this.money--;
+        }
+    }
+});
+</script>
+
+<style scoped lang="scss"></style>
